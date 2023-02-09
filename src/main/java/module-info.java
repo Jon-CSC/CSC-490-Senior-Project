10d82120b82a764f46df9c5d368ad1cd192bf0a9
jmartinez0 <jmartinez.16c@gmail.com>
2023-02-09 10:09:49 -0500

    Added Firestore dependencies

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.seniorproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.seniorproject to javafx.fxml;
-    exports com.mycompany.seniorproject;
-}
