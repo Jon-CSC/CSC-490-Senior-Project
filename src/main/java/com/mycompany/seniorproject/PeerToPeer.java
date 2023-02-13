14e98c1394efe151ed46efed0f59f729628f6853
Jon-CSC <98854693+Jon-CSC@users.noreply.github.com>
2023-02-13 11:35:13 -0500

    Added initial PeerToPeer methods

---
+++ src/main/java/com/mycompany/seniorproject/PeerToPeer.java
@@ -0,0 +1,100 @@
+package com.mycompany.seniorproject;
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * @author Jonathan Espinal
+ */
+public class PeerToPeer {
+
+    private ServerSocket hostSocket;
+    private Socket clientSocket; 
+    private BufferedReader inputStream;
+    private PrintWriter outputStream;
+
+    /**
+     * Starts a connection for a client to connect to.
+     *
+     * @param port The port to listen on for incoming connections.
+     */
+    public void startHost(int port) {
+        try {
+            hostSocket = new ServerSocket(port);
+            clientSocket = hostSocket.accept(); // process will block here waiting for client request
+            inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outputStream = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(PeerToPeer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Connect to designated IP address and port as client.
+     *
+     * @param ip The host IP to connect to.
+     * @param port The port to connect to on the host.
+     */
+    public void connectToHost(String ip, int port) {
+        try {
+            clientSocket = new Socket(ip, port);
+            inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outputStream = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(PeerToPeer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Read incoming data.
+     * @return 
+     */
+    public String readPacket() {
+        try {
+            String data;
+            // process will block here waiting for input
+            while ((data = inputStream.readLine()) != null) {
+                // TODO: Handle incoming messages.
+            }
+            return data;
+        } catch (IOException ex) {
+            Logger.getLogger(PeerToPeer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    /**
+     * Send a message across the connection
+     *
+     * @param message The message to send.
+     */
+    public void sendPacket(String message) {
+        outputStream.write(message + "\n");
+    }
+
+    /**
+     * Close all open streams and client/host connections.
+     */
+    public void closeConnection() {
+        try {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (hostSocket != null) {
+                hostSocket.close();
+            }
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PeerToPeer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
