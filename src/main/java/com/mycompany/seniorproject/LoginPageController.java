1053527ea160381155f60222ce876c408f9d7894
jmartinez0 <89937578+jmartinez0@users.noreply.github.com>
2023-02-27 20:08:43 -0500

    Removed unnecessary code to switch stages

--- src/main/java/com/mycompany/seniorproject/LoginPageController.java
+++ src/main/java/com/mycompany/seniorproject/LoginPageController.java
@@ -74,17 +74,6 @@ public class LoginPageController implements Initializable {
                 errorLabel.setText("Password is incorrect");
                 errorLabel.setVisible(true);
             }
-            /*
-            Stage oldStage = (Stage) usernameField.getScene().getWindow();
-            oldStage.close();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("ThisIsTheFxmlFile.fxml"));
-            Parent parent = loader.load();
-            Stage newStage = new Stage();
-            Scene scene = new Scene(parent, 960, 540);
-            newStage.setTitle("This is the title");
-            newStage.setScene(scene);
-            newStage.show();
-            */
         } catch (FirebaseAuthException ex) {
             if (passwordField.getText().equals("")) {
                 errorLabel.setText("All fields must be filled out");
