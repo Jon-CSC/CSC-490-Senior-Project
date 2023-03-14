f00e1ccb1dfc77d2c1e7c11454cbe298fe4b0c68
Christopher Canenguez <canenguez.chris@gmail.com>
2023-03-13 20:39:07 -0400

    Changed the css for forgotPasswordField to match project.

--- src/main/java/com/mycompany/seniorproject/ForgotPasswordPageController.java
+++ src/main/java/com/mycompany/seniorproject/ForgotPasswordPageController.java
@@ -4,12 +4,13 @@ package com.mycompany.seniorproject;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -18,18 +19,27 @@ import javafx.fxml.Initializable;
  */
 public class ForgotPasswordPageController implements Initializable {
 
+    @FXML
+    private TextField usernameField;
+    
+    @FXML
+    private PasswordField newPasswordField;
+    
+    @FXML
+    private PasswordField confirmPasswordField;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
     @FXML
     public void goToLoginPage() throws IOException {
         // Retrieves Loader for Login page.
         App.setRoot("LoginPage");
     } // End goToLoginPage.
-    
+
 }
