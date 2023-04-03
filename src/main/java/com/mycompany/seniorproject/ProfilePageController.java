c66f289165bd76064fa8404a6e88ab937292bfdb
jmartinez0 <jmartinez.16c@gmail.com>
2023-04-03 04:38:58 -0400

    Profile page front end complete

--- src/main/java/com/mycompany/seniorproject/ProfilePageController.java
+++ src/main/java/com/mycompany/seniorproject/ProfilePageController.java
@@ -1,15 +1,22 @@
 package com.mycompany.seniorproject;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  *
@@ -18,19 +25,29 @@ import javafx.scene.image.ImageView;
 public class ProfilePageController implements Initializable {
     
     static String userID;
-    
-    @FXML private ImageView profilePicture;
-    @FXML private Label username, bio;
-    @FXML private Hyperlink bioEditLink, avatarEditLink;
-
+    final int MAX_CHARS_BIO = 150;
+    @FXML private Label username, characterCounter;
+    @FXML private Rectangle profilePicRectangle, game1Rectangle, game2Rectangle, game3Rectangle, game4Rectangle;
+    @FXML private Circle editButtonCircle, cancelEditCircle, profilePicEditButton;
+    @FXML private TextArea bioTextArea;
     @FXML private Label snakePlaytime, snakeHiscore, battleshipPlaytime, battleshipWins,
                  checkersPlaytime, checkersWins, tictactoePlaytime, tictactoeWins;
             
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // in future, it'd be a good idea to check OS and dynamically update game images instead
-        // for now, i just set them in the FXML
+        bioTextArea.setEditable(false);
+        profilePicEditButton.setVisible(false);
+        editButtonCircle.setVisible(false);
+        cancelEditCircle.setVisible(false);
+        characterCounter.setVisible(false);
+        trackBioCharacterCount();
         fillInProfileData();
+        loadImages();
+    }
+
+    @FXML
+    public void goToGameLibrary() throws IOException {
+        App.setRoot("gameLibrary");
     }
     
     // pass the controller the user whose profile you want to see
@@ -44,6 +61,7 @@ public class ProfilePageController implements Initializable {
             // fall back to the local account if there's a misconfiguration
             userID = LocalUserAccount.getInstance().getUser().getUserID();
         }
+
         UserAccount profileUser = UserAccount.downloadUser(userID, App.fstore);
         if(null == profileUser) {
             return; // maybe show the user an error here?
@@ -51,27 +69,21 @@ public class ProfilePageController implements Initializable {
         
         // if it's the local account, make the edit links visible and add their logic
         if(LocalUserAccount.getInstance().getUser().getUserID().equals(profileUser.getUserID())) {
-            bioEditLink.setOnMouseClicked(e -> {
-                System.out.println("editing bio :D");
-            });
-            avatarEditLink.setOnMouseClicked(e -> {
-                System.out.println("editing avatar :D");
-            });
-            bioEditLink.setVisible(true);
-            avatarEditLink.setVisible(true);
+            editButtonCircle.setVisible(true);
         }
         
         // fill in their personal info
         username.setText(profileUser.getUserID());
-        bio.setText(profileUser.getBiography());
+        bioTextArea.setText(profileUser.getBiography());
         Image profilePic;
         try {
             URL avatarURL = new URL(profileUser.getAvatarURL());
             profilePic = new Image(avatarURL.toString());
+            profilePicRectangle.setFill(new ImagePattern(profilePic));
         } catch (IOException ex) {
-            profilePic = new Image(getClass().getResourceAsStream("Images\\penguin01.jpg"));
+            profilePic = new Image(getClass().getResourceAsStream("Images/penguin01.jpg"));
+            profilePicRectangle.setFill(new ImagePattern(profilePic));
         }
-        profilePicture.setImage(profilePic);
         
         // fill in their stats
         HashMap<String, Object> gameData = profileUser.getGameData();
@@ -84,4 +96,102 @@ public class ProfilePageController implements Initializable {
         tictactoePlaytime.setText(gameData.getOrDefault(UserAccount.TICTACTOE_TIME, 0).toString());
         tictactoeWins.setText(gameData.getOrDefault(UserAccount.TICTACTOE_WINS, 0).toString());
     }
+
+    /**
+     * Loads all the images on a user's profile page.
+     */
+    private void loadImages() {
+        Image game1, game2, game3, game4, editButton, cancelEditButton;
+        game1 = new Image(getClass().getResourceAsStream("Images/tictactoe.png"));
+        game2 = new Image(getClass().getResourceAsStream("Images/snake.png"));
+        game3 = new Image(getClass().getResourceAsStream("Images/battleship.png"));
+        game4 = new Image(getClass().getResourceAsStream("Images/checkers.png"));
+        editButton = new Image(getClass().getResourceAsStream("Images/edit.png"));
+        cancelEditButton = new Image(getClass().getResourceAsStream("Images/cancel.png"));
+        game1Rectangle.setFill(new ImagePattern(game1));
+        game2Rectangle.setFill(new ImagePattern(game2));
+        game3Rectangle.setFill(new ImagePattern(game3));
+        game4Rectangle.setFill(new ImagePattern(game4));
+        editButtonCircle.setFill(new ImagePattern(editButton));
+        cancelEditCircle.setFill(new ImagePattern(cancelEditButton));
+    }
+
+    /**
+     * Allows the user to edit their profile.
+     */
+    @FXML
+    public void editProfile() {
+        Image editButton;
+        //If the user is not already editing and wants to make changes using the pencil icon
+        if (!bioTextArea.isEditable()) {
+            bioTextArea.setEditable(true);
+            bioTextArea.requestFocus();
+            bioTextArea.positionCaret(bioTextArea.getText().length());
+            //This is to change the content of the text area
+            Region content = (Region) bioTextArea.lookup(".content");
+            content.setStyle("-fx-background-color: #353535");
+            content.setStyle("-fx-cursor: text");
+            profilePicEditButton.setVisible(true);
+            cancelEditCircle.setVisible(true);
+            characterCounter.setVisible(true);
+            editButton = new Image(getClass().getResourceAsStream("Images/checkmark.png"));
+            editButtonCircle.setFill(new ImagePattern(editButton));
+        } else {
+            //If the user was already editing and saved their changes using the checkmark icon
+            bioTextArea.setEditable(false);
+            profilePicEditButton.setVisible(false);
+            cancelEditCircle.setVisible(false);
+            characterCounter.setVisible(false);
+            //This is to change the content of the text area
+            Region content = (Region) bioTextArea.lookup(".content");
+            content.setStyle("-fx-background-color: #232323");
+            content.setStyle("-fx-cursor: default");
+            editButton = new Image(getClass().getResourceAsStream("Images/edit.png"));
+            editButtonCircle.setFill(new ImagePattern(editButton));
+
+            // code to take what's in the text area and update the user's bio in firestore
+            // code to get the updated version and update the bio label and bioTextArea
+        }
+    }
+
+    /**
+     * Cancels any changes made while editing the user's profile.
+     */
+    @FXML
+    public void cancelEditProfile() {
+        Image editButton;
+        bioTextArea.setEditable(false);
+        profilePicEditButton.setVisible(false);
+        cancelEditCircle.setVisible(false);
+        characterCounter.setVisible(false);
+        //This is to change the content of the text area
+        Region content = (Region) bioTextArea.lookup(".content");
+        content.setStyle("-fx-background-color: #232323");
+        content.setStyle("-fx-cursor: default");
+        editButton = new Image(getClass().getResourceAsStream("Images/edit.png"));
+        editButtonCircle.setFill(new ImagePattern(editButton));
+
+        // cancel the changes and get the user's bio (it'll be the same bio it was before they began editing)
+    }
+
+    /**
+     * Tracks the amount of characters a user has in their bio.
+     */
+    private void trackBioCharacterCount() {
+        bioTextArea.setTextFormatter(new TextFormatter<String>(
+                change -> change.getControlNewText().length() <= MAX_CHARS_BIO ? change : null));
+        characterCounter.textProperty().bind(bioTextArea.textProperty().length().asString("%d"));
+        characterCounter.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> ov, String t, String t1) {
+                //If the user hits the 150 char limit the counter will turn red
+                if (characterCounter.getText().equals("150")) {
+                    characterCounter.setStyle("-fx-text-fill: #de4a3b");
+                } else {
+                    characterCounter.setStyle("-fx-text-fill: white");
+                }
+            }
+        });
+    }
+
 }
