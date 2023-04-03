767bb1de4e8da8c787c29901b6d38ac99e15422d
Justin Karp <62733126+KiwiCharged@users.noreply.github.com>
2023-04-02 22:25:03 -0400

    Update SnakeGameController.java

--- src/main/java/com/mycompany/seniorproject/games/snake/SnakeGameController.java
+++ src/main/java/com/mycompany/seniorproject/games/snake/SnakeGameController.java
@@ -1,4 +1,35 @@
 /*
+
+package com.mycompany.seniorproject.games;
+
+import com.mycompany.seniorproject.App;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+
+public class SnakeGameController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    void testClick(MouseEvent event) throws IOException {
+        System.out.println("SnakeController works!");
+        App.setRoot("gameLibrary");
+    }
+}
+
+*/
+
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
