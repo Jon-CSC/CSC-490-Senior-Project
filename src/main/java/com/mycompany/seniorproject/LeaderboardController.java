683f09e8503ba6f900ed128eb07e41bce9672ac1
jmartinez0 <jmartinez.16c@gmail.com>
2023-04-22 00:34:38 -0400

    Created leaderboard scene

---
+++ src/main/java/com/mycompany/seniorproject/LeaderboardController.java
@@ -0,0 +1,22 @@
+package com.mycompany.seniorproject;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller Class
+ *
+ * @author juan
+ */
+public class LeaderboardController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+}
