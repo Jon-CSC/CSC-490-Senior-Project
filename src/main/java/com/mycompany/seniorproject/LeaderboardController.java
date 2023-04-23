30a99105889a104f5a619bfe6c532f03cff48fb2
jmartinez0 <jmartinez.16c@gmail.com>
2023-04-22 20:38:07 -0400

    Leaderboard UI complete

--- src/main/java/com/mycompany/seniorproject/LeaderboardController.java
+++ src/main/java/com/mycompany/seniorproject/LeaderboardController.java
@@ -1,7 +1,10 @@
 package com.mycompany.seniorproject;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,10 +15,18 @@ import java.util.ResourceBundle;
  */
 public class LeaderboardController implements Initializable {
 
+    @FXML private Button snakeButton, javastroidsButton, ticTacToeButton, battleshipButton, chessButtons, checkersButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        App.getStage().setWidth(900);
+        App.getStage().setHeight(640);
+        App.getStage().centerOnScreen();
+    }
 
+    @FXML
+    public void goToGameLibrary() throws IOException {
+        App.setRoot("gameLibrary");
     }
 
 
