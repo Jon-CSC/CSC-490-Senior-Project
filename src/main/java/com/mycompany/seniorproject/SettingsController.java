9e23fb49402fcb6e895395716f3c124c77b7a030
jmartinez0 <jmartinez.16c@gmail.com>
2023-05-04 15:45:51 -0400

    Removed forgot password scene and added settings scene

---
+++ src/main/java/com/mycompany/seniorproject/SettingsController.java
@@ -0,0 +1,20 @@
+package com.mycompany.seniorproject;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller Class
+ *
+ * @author juan
+ */
+public class SettingsController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
