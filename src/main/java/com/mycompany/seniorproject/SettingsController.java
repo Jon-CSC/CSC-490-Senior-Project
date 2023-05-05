4b8a1a1af97acce81ddc698148ec70eee539e684
jmartinez0 <jmartinez.16c@gmail.com>
2023-05-05 01:39:06 -0400

    Settings main UI elements done

--- src/main/java/com/mycompany/seniorproject/SettingsController.java
+++ src/main/java/com/mycompany/seniorproject/SettingsController.java
@@ -1,7 +1,9 @@
 package com.mycompany.seniorproject;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,6 +16,16 @@ public class SettingsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void goToGameLibrary() throws IOException {
+        App.setRoot("gameLibrary");
+    }
+
+    @FXML
+    public void showInfo() {
 
     }
 
