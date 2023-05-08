b80bbc6710e5559419c5663032c6c125ac44ca1a
jmartinez0 <jmartinez.16c@gmail.com>
2023-05-08 19:59:58 -0400

    Added nanoclip icon across the app

--- src/main/java/com/mycompany/seniorproject/App.java
+++ src/main/java/com/mycompany/seniorproject/App.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -28,7 +29,8 @@ public class App extends Application {
         fstore = firestore();
         scene = new Scene(loadFXML("LoginPage"));
         timer = null;
-        stage.setTitle("Minigame App");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("Images/nanoclip.png")));
+        stage.setTitle("NanoClip");
         stage.setScene(scene);
         stage.show();
     }
